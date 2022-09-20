9d3e1fa1c7ccb2b2fdbc891322b45f85acc0fa7a
Ferchus <ferchusgames@protonmail.com>
2022-09-20 09:05:37 -0500

    Added A Manager Resetter

---
+++ QuestUp/Assets/[My Assets]/[Scripts]/Controllers/ManagerInstanceDestroyer.cs
@@ -0,0 +1,22 @@
+using QuestUp;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerInstanceDestroyer : MonoBehaviour
+{
+    private void Awake()
+    {
+        DestroyManagerInstance(HealthManager.Instance.gameObject);
+        DestroyManagerInstance(CoinManager.Instance.gameObject);
+        DestroyManagerInstance(GameManager.Instance.gameObject);
+    }
+
+    private void DestroyManagerInstance(GameObject gameObject)
+    {
+        if (gameObject != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
