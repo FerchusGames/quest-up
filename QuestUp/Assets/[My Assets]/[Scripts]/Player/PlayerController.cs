016cbe4117919fa9dd155607797b8bccb2b3db37
Ferchus <98918903+FerchusGames@users.noreply.github.com>
2022-10-17 07:48:07 -0500

    Created first level and hazard (#4)
    * Before Importing Touch Samples
    
    * Before fixing Input System
    
    * Joystick debugs value
    
    * Implemented movement
    
    Player now moves with WASD and Left Joystick using the new Input System
    
    * Created ProjectileSO
    
    * Projectile movement is dependent on SO
    
    The MoveRight function now gets its _movementSpeed from a ProjectileSO
    
    * Added Sprites To Enemies
    
    * Added Base, Border, and Lava Tilemaps
    
    * Added laser beam
    
    * Added Laser Generator
    
    * Initialized null variable

---
+++ QuestUp/Assets/[My Assets]/[Scripts]/Player/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace QuestUp
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private float _movementSpeed;
+
+        private PlayerInput _playerInput = null;
+        private Rigidbody2D _rigidbody2D = null;
+        private PlayerInputActions _playerInputActions = null;
+        private Vector2 _moveDirection = Vector2.zero;
+
+        private void Awake()
+        {
+            _playerInput = GetComponent<PlayerInput>();
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+
+            _playerInputActions = new PlayerInputActions();
+            _playerInputActions.Player.Enable();
+
+            _playerInputActions.Player.Move.performed += MoveInput;
+            _playerInputActions.Player.Move.canceled += MoveInput;
+        }
+
+        private void MoveInput(InputAction.CallbackContext context)
+        {
+            _moveDirection = context.ReadValue<Vector2>();
+            _rigidbody2D.velocity = _moveDirection * _movementSpeed;
+            //Debug.Log(context.ReadValue<Vector2>());
+        }
+
+
+        private void OnEnable()
+        {
+           _playerInputActions.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _playerInputActions.Disable();
+        }
+    }   
+}
---
+++ QuestUp/Assets/[My Assets]/[Scripts]/Projectiles/MoveRight.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace QuestUp
+{
+    public class MoveRight : MonoBehaviour
+    {
+        [SerializeField] private ProjectileSO _projectileSO = null;
+        private float _movementSpeed = default;
+
+        private void Start()
+        {
+            _movementSpeed = _projectileSO.Speed;
+        }
+
+        void Update()
+        {
+            transform.Translate(Vector2.right * _movementSpeed * Time.deltaTime); 
+        }
+    }
+}
