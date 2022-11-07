b5f1112784d5c3ad214b1a65141f9d429c2ba06b
Ferchus <ferchusgames@protonmail.com>
2022-11-07 13:00:53 -0600

    Implemented Player Animation

--- QuestUp/Assets/[My Assets]/[Scripts]/Player/PlayerController.cs
+++ QuestUp/Assets/[My Assets]/[Scripts]/Player/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,15 +8,25 @@ namespace QuestUp
 {
     public class PlayerController : MonoBehaviour
     {
+        public bool IsFacingRight { get; private set; }
+
         [SerializeField] private float _movementSpeed;
 
+        private Animator _animator = null;
         private PlayerInput _playerInput = null;
         private Rigidbody2D _rigidbody2D = null;
         private PlayerInputActions _playerInputActions = null;
         private Vector2 _moveDirection = Vector2.zero;
 
+        private int _ahAxisX = Animator.StringToHash("AxisX");
+        private int _ahAxisY = Animator.StringToHash("AxisY");
+
         private void Awake()
         {
+            IsFacingRight = true;
+
+            _animator = GetComponent<Animator>();
+
             _playerInput = GetComponent<PlayerInput>();
             _rigidbody2D = GetComponent<Rigidbody2D>();
 
@@ -30,9 +41,30 @@ namespace QuestUp
         {
             _moveDirection = context.ReadValue<Vector2>();
             _rigidbody2D.velocity = _moveDirection * _movementSpeed;
+
+            _animator.SetFloat(_ahAxisX, _moveDirection.x);
+            _animator.SetFloat(_ahAxisY, _moveDirection.y);
+
+            CheckDirectionToFace(_moveDirection.x > 0);
             //Debug.Log(context.ReadValue<Vector2>());
         }
 
+        private void CheckDirectionToFace(bool isMovingRight)
+        {
+            if (isMovingRight != IsFacingRight)
+            {
+                Turn();
+            }
+        }
+
+        private void Turn()
+        {
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+
+            IsFacingRight = !IsFacingRight;
+        }
 
         private void OnEnable()
         {
