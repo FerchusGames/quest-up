76dc69ea5de836a7eb872fcfbef1f683cc627c34
Ferchus <98918903+FerchusGames@users.noreply.github.com>
2022-11-16 20:28:23 -0600

    First Stable Build

--- QuestUp/Assets/[My Assets]/[Scripts]/Managers/HazardManager.cs
+++ QuestUp/Assets/[My Assets]/[Scripts]/Managers/HazardManager.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem.iOS;
-
+    
 namespace QuestUp
 {
     public class HazardManager : MonoBehaviour
